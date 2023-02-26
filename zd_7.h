0f95155e1fcdd65649b1a5f87a06738d4c4a8fb9
kiruxa193 <95447587+kiruxa193@users.noreply.github.com>
2023-02-26 15:29:46 +0300

    Update zd_7.h

--- zd_7.h
+++ zd_7.h
@@ -5,19 +5,11 @@ int zd7() {
     const int N = 5;
     float x = -10.0f/3.0f;
     float Mfs[N] = {x, x, x, x, x};
-    float new_value1;
-    float new_value2;
-    float new_value3;
-    float new_value4;
-    float new_value5;
-    if (scanf("%f %f %f %f %f", &new_value1,&new_value2,&new_value3,&new_value4,&new_value5) != 5) {
+
+    if (scanf("%f %f %f %f %f", &Mfs[0],&Mfs[1],&Mfs[2],&Mfs[3],&Mfs[4]) != 5) {
         printf("Error\n");
     } else {
-        Mfs[0] = new_value1;
-        Mfs[1] = new_value2;
-        Mfs[2] = new_value3;
-        Mfs[3] = new_value4;
-        Mfs[4] = new_value5;
+
         printf("Mass after changing: ");
         for (int i = 0; i < 5; i++) {
             printf("%f ", Mfs[i]);
