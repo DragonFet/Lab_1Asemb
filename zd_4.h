027c172afb00959a9ddaaf66bd5708fa893f1b57
DragonFet <94041595+DragonFet@users.noreply.github.com>
2023-02-26 14:52:03 +0300

    Volodya Pidor

--- zd_4.h
+++ zd_4.h
@@ -1,39 +1,49 @@
 #include <iostream>
-#include <cstdint>
+#include <stdio.h>
+
 using namespace std;
+
 
 
 int launch4()
 {
-    uint16_t x16=0xDEAD;
-    uint32_t x32=0xC0CAC01A;
-    uint64_t x64=0xC0FFEE11BEA7;
-    uint16_t M16[5] = {x16, x16, x16, x16, x16};
-    uint32_t M32[5] = {x32, x32, x32, x32, x32};
-    uint64_t M64[5] = {x64, x64, x64, x64, x64};
-    const int N = 5;
-    float x = -10.0f/3.0f;
-    double y = -10.0/3.0;
-    float Ms[N] = {x, x, x, x, x};
-    double Ml[N] = {y, y, y, y, y};
-
-    cout<<"x16"<<endl;
-    for (int i = 0; i < 5; ++i) printf("%04X ", M16[i]);
-    cout<<"x32"<<endl;
-    for (int i = 0; i < 5; ++i) printf("%08X ", M32[i]);
-    cout<<"x64"<<endl;
-    for (int i = 0; i < 5; ++i) printf("%016llX ", M64[i]);
+    const int N=5;
+    cout<<endl<<"x16"<<endl;
+    ::uint16_t x1= 0xDEAD;
+    ::uint16_t Ms[N]={x1,x1,x1,x1,x1};
+    for (::uint16_t M : Ms) {
+        printf("%04X", M);
+        cout<<"\t";
+    }
+    cout<<endl<<"x32"<<endl;
+    uint32_t x = 0xC0CAC01A;
+    uint32_t Ml[5] = {x, x, x, x, x};
+    uint64_t Mq[5] = {0xC0FFEE11BEA7, 0xC0FFEE11BEA7, 0xC0FFEE11BEA7, 0xC0FFEE11BEA7, 0xC0FFEE11BEA7};
+    printf("Ml array: ");
+    for (unsigned int i : Ml) {
+        printf("%08X ", i);
+    }
+    printf("\nMq array: ");
+    for (unsigned long long i : Mq) {
+        printf("%016llX ", i);
+    }
+    cout<<endl<<"x32 float "<<endl;
+    float x5 = -10.0f/3.0f;
+    float Mfs[N]={x5,x5,x5,x5,x5};
+    for (float J:Mfs)
+    {
+        std::printf("%f",J);
+        cout<<'\t';
+    }
     cout<<endl;
-    cout << "Ms: ";
-    for (int i = 0; i < N; i++) {
-        printf("%.2f ", Ms[i]);
+    cout<<endl<<"x64 double "<<endl;
+    double y = -10.0/3.0;
+    double Mfl[N]={y,y,y,y,y};
+    for (double L:Mfl)
+    {
+        std::printf("%lf",L);
+        cout<<'\t';
     }
-    cout << endl;
-
-    cout << "Ml: ";
-    for (int i = 0; i < N; i++) {
-        printf("%.2lf ", Ml[i]);
-    }
-    cout << endl;
-
+    cout<<endl;
+    return  0;
 }
